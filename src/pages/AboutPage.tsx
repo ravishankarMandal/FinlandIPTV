8672449e8cc0e6fe016c01062ce9527028eede93
ravishankarMandal <ravishankar42001@gmail.com>
2026-09-24 05:30:22 +0530

    Update

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -9,7 +9,7 @@ export default function AboutUs() {
           1. HERO BANNER
       ========================== */}
       <section
-        className="relative flex h-50 items-center justify-center bg-cover bg-top sm:h-57.5"
+        className="relative flex h-50 items-center justify-center bg-cover sm:h-100"
         style={{
           backgroundImage: `linear-gradient(rgba(5, 25, 45, 0.72), rgba(5, 25, 45, 0.72)), url(${heroSection})`,
         }}
